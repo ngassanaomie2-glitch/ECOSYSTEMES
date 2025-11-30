95cebb99e0c99a59cb4f82fa362a366c74974448
ngassanaomie2-glitch <ngassanaomie2@icloud.com>
2025-11-30 22:54:57 +0100

    Update Time.hpp

--- Time.hpp
+++ Time.hpp
@@ -14,3 +14,27 @@ float Time::GetDelta() const noexcept { return mDelta; }
 
 } // namespace Core
 } // namespace Ecosystem
+Time.cpp
+#ifndef ECOSYSTEM_CORE_TIME_HPP
+#define ECOSYSTEM_CORE_TIME_HPP
+
+#include <chrono>
+
+namespace Ecosystem {
+namespace Core {
+
+class Time {
+public:
+    Time();
+    void Tick();
+    float GetDelta() const noexcept;
+private:
+    using clock = std::chrono::high_resolution_clock;
+    clock::time_point mLast;
+    float mDelta;
+};
+
+} // namespace Core
+} // namespace Ecosystem
+
+#endif // ECOSYSTEM_CORE_TIME_HPP
